67ad98c4a1731c0d12f7de263db2cecf337189ff
Kjetil Golid <kjetil.golid@gmail.com>
2018-05-22 00:44:51 +0200

    bugfix

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ modules.exports = class Justeer {
   placement_given_spacing_between_elements(spacing) {
     let total_size = this.number_of_elements * this.element_size + (this.number_of_elements - 1) * spacing;
     let outer_padding = (this.container_size - total_size) / 2;
-    return i => outer_padding + i * (element_size + spacing);
+    return i => outer_padding + i * (this.element_size + spacing);
   }
 
   placement_given_spacing_around_elements(spacing) {
     let total_size = this.number_of_elements * this.element_size + 2 * spacing;
     let padding_between = (this.container_size - total_size) / (this.number_of_elements - 1);
-    return i => spacing + i * (element_size + padding_between);
+    return i => spacing + i * (this.element_size + padding_between);
   }
 };
